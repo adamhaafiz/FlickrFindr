ca857d22c898876a200f36002720cb4b6e4f9834
Adam Haafiz <adam.haafiz@warnermedia.com>
2023-06-13 16:13:27 +0200

    Revert "Delete tests"
    48cc743d78511893e79f88251ee8358daee29717

---
+++ Tests/MockPhotoRepository.swift
@@ -0,0 +1,16 @@
+@testable import Core
+
+class MockPhotoRepository: PhotoRepositoryProtocol {
+    static var getRecentPhotosCalled: (() -> Void)?
+    static var getPhotosCalled: ((String) -> Void)?
+
+    func getPhotos(_ query: String) async throws -> [Photo] {
+        MockPhotoRepository.getPhotosCalled?(query)
+        return [.preview(title: query)]
+    }
+
+    func getRecentPhotos() async throws -> [Photo] {
+        MockPhotoRepository.getRecentPhotosCalled?()
+        return [.preview]
+    }
+}
