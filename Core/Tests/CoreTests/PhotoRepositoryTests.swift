6576ff53f2da7ed398530af49f654b912e899f7b
Adam Haafiz <adam.haafiz@warnermedia.com>
2023-06-13 16:31:27 +0200

    Revert "Remove tests"
    15a80daa2feb77823a3059c2beafb14c1939f23c

--- Core/Tests/CoreTests/PhotoRepositoryTests.swift
+++ Core/Tests/CoreTests/PhotoRepositoryTests.swift
@@ -2,16 +2,16 @@ import XCTest
 @testable import Core
 
 final class PhotoRepositoryTests: XCTestCase {
-//    var sut: PhotoRepository!
-//
-//    override func setUpWithError() throws {
-//        sut = PhotoRepository(api: MockAPI())
-//    }
-//
-//    func testRepository_shouldReturnPhotosFromAPI() async throws {
-//        let photos = try await sut.getPhotos("test query")
-//
-//        XCTAssertFalse(photos.isEmpty)
-//        XCTAssertEqual(photos.first!.id, 52703925845)
-//    }
+    var sut: PhotoRepository!
+
+    override func setUpWithError() throws {
+        sut = PhotoRepository(api: MockAPI())
+    }
+
+    func testRepository_shouldReturnPhotosFromAPI() async throws {
+        let photos = try await sut.getPhotos("test query")
+
+        XCTAssertFalse(photos.isEmpty)
+        XCTAssertEqual(photos.first!.id, 52703925845)
+    }
 }
